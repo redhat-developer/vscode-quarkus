e50055b7a11fa4cfa9cc1d2113a5f5fb6aba2887
David Kwon <dakwon@redhat.com>
2019-07-31 13:27:29 -0400

    Rename request command and LS jar name
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/definitions/commandConstants.ts
+++ src/definitions/commandConstants.ts
@@ -1,5 +1,5 @@
 // Quarkus language server request and notifiactions
-export const QUARKUS_PROPERTIES_REQUEST = 'quarkus/properties';
+export const QUARKUS_PROJECT_REQUEST = 'quarkus/projectInfo';
 
 // Quarkus jdt.ls extension commands
 export const JDTLS_PROJECT_INFO_COMMAND = 'quarkus.java.projectInfo';
