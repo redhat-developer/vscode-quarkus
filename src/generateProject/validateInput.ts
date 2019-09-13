295d8384061334ee4dbe8f71db055512df7c7afb
David Kwon <dakwon@redhat.com>
2019-09-13 12:55:41 -0400

    Update project generation input validation
    Signed-off-by: David Kwon <dakwon@redhat.com>

---
+++ src/generateProject/validateInput.ts
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2019 Red Hat, Inc. and others.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// For validation functions, return error message if there is an error
+// return undefined otherwise
+
+export async function validateGroupId(userInput: string): Promise<string|undefined> {
+  // regex referenced from
+  // https://github.com/quarkusio/code.quarkus.io/blob/master/src/main/frontend/src/code-quarkus/pickers/info-picker.tsx
+  const re = new RegExp('^([a-zA-Z_$][a-zA-Z\\d_$]*\\.)*[a-zA-Z_$][a-zA-Z\\d_$]*$');
+  if (!re.test(userInput)) {
+    return 'Invalid groupId';
+  }
+  return undefined;
+}
+
+export async function validateArtifactId(userInput: string): Promise<string|undefined> {
+  // regex referenced from
+  // https://github.com/quarkusio/code.quarkus.io/blob/master/src/main/frontend/src/code-quarkus/pickers/info-picker.tsx
+  const re = new RegExp('^[a-z][a-z0-9-._]*$');
+  if (!re.test(userInput)) {
+    return 'Invalid artifactId';
+  }
+  return undefined;
+}
+
+export async function validateVersion(userInput: string): Promise<string|undefined> {
+  const re = new RegExp('[A-Za-z0-9_\\-.]+$');
+  if (!re.test(userInput)) {
+    return 'Invalid project version';
+  }
+  return undefined;
+}
+
+export async function validatePackageName(userInput: string): Promise<string|undefined> {
+  // regex referenced from
+  // https://github.com/quarkusio/code.quarkus.io/blob/master/src/main/frontend/src/code-quarkus/pickers/info-picker.tsx
+  const re = new RegExp('^([a-zA-Z_$][a-zA-Z\\d_$]*\\.)*[a-zA-Z_$][a-zA-Z\\d_$]*$');
+  if (!re.test(userInput)) {
+    return 'Invalid package name';
+  }
+  return undefined;
+}
+
+export async function validateResourceName(userInput: string): Promise<string|undefined> {
+  const re = new RegExp('^[A-Za-z]+[A-Za-z0-9_.]*$');
+  if (!re.test(userInput)) {
+    return 'Invalid resource name';
+  }
+  return undefined;
+}
