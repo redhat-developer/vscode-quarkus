0feaa2ffef5592099ffec6c271f823f5e4cb53bb
azerr <azerr@redhat.com>
2022-05-19 15:00:34 +0200

    Move to vscode-languageclient 8.0.2 for Qute LS
    Fixes #505
    
    Signed-off-by: azerr <azerr@redhat.com>

--- src/qute/languageServer/client.ts
+++ src/qute/languageServer/client.ts
@@ -1,130 +1,121 @@
-import * as requirements from './requirements';
+import { resolveRequirements } from './requirements';
 
 import { DidChangeConfigurationNotification, LanguageClientOptions } from 'vscode-languageclient';
 import { LanguageClient } from 'vscode-languageclient/node';
-import { ExtensionContext, commands, workspace, window, ConfigurationTarget, languages } from 'vscode';
+import { ExtensionContext, commands, workspace, window, ConfigurationTarget } from 'vscode';
 import { prepareExecutable } from './quteServerStarter';
 import { registerQuteExecuteWorkspaceCommand, registerVSCodeQuteCommands, synchronizeQuteValidationButton } from '../commands/registerCommands';
 import { QuteClientCommandConstants } from '../commands/commandConstants';
 import { QuteSettings } from './settings';
 import { JavaExtensionAPI } from '../../extension';
-import { QuteInlayHintsProvider } from './inlayHintsProvider';
 
-export function connectToQuteLS(context: ExtensionContext, api: JavaExtensionAPI) {
+export async function connectToQuteLS(context: ExtensionContext, api: JavaExtensionAPI) {
   registerVSCodeQuteCommands(context);
+  const requirements = await resolveRequirements(api);
 
-  return requirements.resolveRequirements(api).then(requirements => {
-    const clientOptions: LanguageClientOptions = {
-      documentSelector: [
-        { scheme: 'file', language: 'qute-html' },
-        { scheme: 'file', language: 'qute-json' },
-        { scheme: 'file', language: 'qute-yaml' },
-        { scheme: 'file', language: 'qute-txt' },
-        { scheme: 'untitled', language: 'qute-html' },
-        { scheme: 'vscode-notebook-cell', language: 'qute-html' },
-        { scheme: 'file', language: 'java' }
-      ],
-      // wrap with key 'settings' so it can be handled same a DidChangeConfiguration
-      initializationOptions: {
-        settings: getQuteSettings(),
-        extendedClientCapabilities: {
-          commands: {
-            commandsKind: {
-              valueSet: [
-                QuteClientCommandConstants.OPEN_URI,
-                QuteClientCommandConstants.JAVA_DEFINTION,
-                QuteClientCommandConstants.COMMAND_CONFIGURATION_UPDATE
-              ]
-            }
-          },
-          shouldLanguageServerExitOnShutdown: true
-        }
-      },
-      synchronize: {
-        // preferences starting with these will trigger didChangeConfiguration
-        configurationSection: ['qute']
-      },
-      middleware: {
-        workspace: {
-          didChangeConfiguration: async () => {
-            // A settings.json is updated:
-            // 1. send the new Quet settings to the Qute language server
-            quteLanguageClient.sendNotification(DidChangeConfigurationNotification.type, { settings: getQuteSettings() });
-            // 2. synchronize the Qute toggle button for validation
-            await synchronizeQuteValidationButton(window.activeTextEditor);
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: 'file', language: 'qute-html' },
+      { scheme: 'file', language: 'qute-json' },
+      { scheme: 'file', language: 'qute-yaml' },
+      { scheme: 'file', language: 'qute-txt' },
+      { scheme: 'untitled', language: 'qute-html' },
+      { scheme: 'vscode-notebook-cell', language: 'qute-html' },
+      { scheme: 'file', language: 'java' }
+    ],
+    // wrap with key 'settings' so it can be handled same a DidChangeConfiguration
+    initializationOptions: {
+      settings: getQuteSettings(),
+      extendedClientCapabilities: {
+        commands: {
+          commandsKind: {
+            valueSet: [
+              QuteClientCommandConstants.OPEN_URI,
+              QuteClientCommandConstants.JAVA_DEFINTION,
+              QuteClientCommandConstants.COMMAND_CONFIGURATION_UPDATE
+            ]
           }
         }
       }
-    };
-
-    function bindQuteRequest(request: string) {
-      quteLanguageClient.onRequest(request, async (params: any) =>
-        <any>await commands.executeCommand("java.execute.workspaceCommand", request, params)
-      );
+    },
+    synchronize: {
+      // preferences starting with these will trigger didChangeConfiguration
+      configurationSection: ['qute']
+    },
+    middleware: {
+      workspace: {
+        didChangeConfiguration: async () => {
+          // A settings.json is updated:
+          // 1. send the new Quet settings to the Qute language server
+          const result = quteLanguageClient.sendNotification(DidChangeConfigurationNotification.type, { settings: getQuteSettings() });
+          // 2. synchronize the Qute toggle button for validation
+          await synchronizeQuteValidationButton(window.activeTextEditor);
+          return result;
+        }
+      }
     }
+  };
 
-    function bindQuteNotification(notification: string) {
-      context.subscriptions.push(commands.registerCommand(notification, (event: any) => {
-        quteLanguageClient.sendNotification(notification, event);
-      }));
-    }
+  function bindQuteRequest(request: string) {
+    quteLanguageClient.onRequest(request, async (params: any) =>
+      <any>await commands.executeCommand("java.execute.workspaceCommand", request, params)
+    );
+  }
 
-    const serverOptions = prepareExecutable(requirements);
-    const quteLanguageClient = new LanguageClient('qute', 'Qute Support', serverOptions, clientOptions);
-    context.subscriptions.push(quteLanguageClient.start());
+  function bindQuteNotification(notification: string) {
+    context.subscriptions.push(commands.registerCommand(notification, (event: any) => {
+      quteLanguageClient.sendNotification(notification, event);
+    }));
+  }
 
-    return quteLanguageClient.onReady().then(async () => {
-      bindQuteRequest('qute/template/project');
-      bindQuteRequest('qute/template/projectDataModel');
-      bindQuteRequest('qute/template/userTags');
-      bindQuteRequest('qute/template/javaTypes');
-      bindQuteRequest('qute/template/resolvedJavaType');
-      bindQuteRequest('qute/template/javaDefinition');
-      bindQuteRequest('qute/template/javadoc');
-      bindQuteRequest('qute/template/generateMissingJavaMember');
-      bindQuteRequest('qute/java/codeLens');
-      bindQuteRequest('qute/java/diagnostics');
-      bindQuteRequest('qute/java/documentLink');
-      bindQuteNotification('qute/dataModelChanged');
+  const serverOptions = prepareExecutable(requirements);
+  const quteLanguageClient = new LanguageClient('qute', 'Qute Support', serverOptions, clientOptions);
+  await quteLanguageClient.start();
 
-      registerQuteExecuteWorkspaceCommand(context, quteLanguageClient);
-      // Refresh the Qute context when editor tab has the focus
-      context.subscriptions.push(
-        window.onDidChangeActiveTextEditor(async editor => {
-          await synchronizeQuteValidationButton(editor);
-        })
-      );
-      // Refresh the Qute context when the language id changed (HTML -> Qute HTML or Qute HTML -> HTML)
-      context.subscriptions.push(
-        workspace.onDidOpenTextDocument(async (document) => {
-          // when settings.json is updated, onDidOpenTextDocument is called,
-          // the Qute context must be refreshed only for the activate text editor.
-          if (window.activeTextEditor?.document === document) {
-            await synchronizeQuteValidationButton(window.activeTextEditor);
-          }
-          // Display the experimental Qute validation pop-up if it hasn't been displayed and a Qute file is open
-          if (!hasShownQuteValidationPopUp(context) && document.languageId.includes('qute')) {
-            showQuteValidationPopUp(context);
-          }
-        })
-      );
+  bindQuteRequest('qute/template/project');
+  bindQuteRequest('qute/template/projectDataModel');
+  bindQuteRequest('qute/template/userTags');
+  bindQuteRequest('qute/template/javaTypes');
+  bindQuteRequest('qute/template/resolvedJavaType');
+  bindQuteRequest('qute/template/javaDefinition');
+  bindQuteRequest('qute/template/javadoc');
+  bindQuteRequest('qute/template/generateMissingJavaMember');
+  bindQuteRequest('qute/java/codeLens');
+  bindQuteRequest('qute/java/diagnostics');
+  bindQuteRequest('qute/java/documentLink');
+  bindQuteNotification('qute/dataModelChanged');
+
+  registerQuteExecuteWorkspaceCommand(context, quteLanguageClient);
+  // Refresh the Qute context when editor tab has the focus
+  context.subscriptions.push(
+    window.onDidChangeActiveTextEditor(async editor => {
+      await synchronizeQuteValidationButton(editor);
+    })
+  );
+  // Refresh the Qute context when the language id changed (HTML -> Qute HTML or Qute HTML -> HTML)
+  context.subscriptions.push(
+    workspace.onDidOpenTextDocument(async (document) => {
+      // when settings.json is updated, onDidOpenTextDocument is called,
+      // the Qute context must be refreshed only for the activate text editor.
+      if (window.activeTextEditor?.document === document) {
+        await synchronizeQuteValidationButton(window.activeTextEditor);
+      }
       // Display the experimental Qute validation pop-up if it hasn't been displayed and a Qute file is open
-      if (!hasShownQuteValidationPopUp(context)) {
-        for (const textDocument of workspace.textDocuments) {
-          if (textDocument.languageId.includes('qute')) {
-            showQuteValidationPopUp(context);
-          }
-        }
+      if (!hasShownQuteValidationPopUp(context) && document.languageId.includes('qute')) {
+        showQuteValidationPopUp(context);
       }
-      await setQuteValidationEnabledContext();
-      await synchronizeQuteValidationButton(window.activeTextEditor);
-
-      const supportRegisterInlayHintsProvider = (languages as any).registerInlayHintsProvider;
-      if (supportRegisterInlayHintsProvider) {
-        context.subscriptions.push(languages.registerInlayHintsProvider(clientOptions.documentSelector, new QuteInlayHintsProvider(quteLanguageClient)));
+    })
+  );
+  // Display the experimental Qute validation pop-up if it hasn't been displayed and a Qute file is open
+  if (!hasShownQuteValidationPopUp(context)) {
+    for (const textDocument of workspace.textDocuments) {
+      if (textDocument.languageId.includes('qute')) {
+        showQuteValidationPopUp(context);
       }
-    });
-  });
+    }
+  }
+  await setQuteValidationEnabledContext();
+  await synchronizeQuteValidationButton(window.activeTextEditor);
 }
 
 /**
