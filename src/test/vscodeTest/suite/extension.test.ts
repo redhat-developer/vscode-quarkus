a1f2b2008cee1657c84c4f21de8a245e8c523b42
David Thompson <davthomp@redhat.com>
2022-12-12 11:09:26 -0500

    Fix test failures due to VSCode API change
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- src/test/vscodeTest/suite/extension.test.ts
+++ src/test/vscodeTest/suite/extension.test.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 import { VSCodeCommands } from '../../../definitions/constants';
+import { QuteClientCommandConstants } from '../../../qute/commands/commandConstants';
 
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
@@ -16,8 +17,21 @@ describe('VS Code extension tests', () => {
     const activationCommands = packageJSON.activationEvents.filter((s) => s.startsWith('onCommand:'))
         .map((s) => s.substring('onCommand:'.length));
     const QUARKUS_ACTIVATION_COMMANDS: string[] = [
+      // from package.json
       VSCodeCommands.CREATE_PROJECT,
-      VSCodeCommands.QUARKUS_WELCOME
+      VSCodeCommands.QUARKUS_WELCOME,
+      // added by vscode, since all commands implicitly trigger extension activation
+      // note that there are duplicate entries
+      VSCodeCommands.CREATE_PROJECT,
+      VSCodeCommands.ADD_EXTENSIONS,
+      VSCodeCommands.DEBUG_QUARKUS_PROJECT,
+      `${VSCodeCommands.DEBUG_QUARKUS_PROJECT}${VSCodeCommands.SHORT_SUFFIX}`,
+      VSCodeCommands.BUILD_BINARY,
+      VSCodeCommands.QUARKUS_WELCOME,
+      VSCodeCommands.DEPLOY_TO_OPENSHIFT,
+      `${VSCodeCommands.DEPLOY_TO_OPENSHIFT}${VSCodeCommands.SHORT_SUFFIX}`,
+      QuteClientCommandConstants.QUTE_VALIDATION_ENABLED_TOGGLE_OFF,
+      QuteClientCommandConstants.QUTE_VALIDATION_ENABLED_TOGGLE_ON,
     ];
 
     expect(activationCommands).to.have.members(QUARKUS_ACTIVATION_COMMANDS);
