d3e02239babe4cf5232128699de41cbf24de2994
Fred Bricon <fbricon@gmail.com>
2023-04-04 20:03:51 +0200

    Update vscode-redhat-telemetry to 0.6.1
    - Also bump webpack to 5.76.0
    
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/test/vscodeTest/suite/extension.test.ts
+++ src/test/vscodeTest/suite/extension.test.ts
@@ -1,10 +1,9 @@
-import * as vscode from 'vscode';
-
-import { VSCodeCommands } from '../../../definitions/constants';
-import { QuteClientCommandConstants } from '../../../qute/commands/commandConstants';
-
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import * as vscode from 'vscode';
+import { VSCodeCommands } from '../../../definitions/constants';
+
+
 
 describe('VS Code extension tests', () => {
 
@@ -20,21 +19,6 @@ describe('VS Code extension tests', () => {
       // from package.json
       VSCodeCommands.CREATE_PROJECT,
       VSCodeCommands.QUARKUS_WELCOME,
-      // added by vscode, since all commands implicitly trigger extension activation
-      // note that there are duplicate entries
-      VSCodeCommands.CREATE_PROJECT,
-      VSCodeCommands.ADD_EXTENSIONS,
-      VSCodeCommands.DEBUG_QUARKUS_PROJECT,
-      `${VSCodeCommands.DEBUG_QUARKUS_PROJECT}${VSCodeCommands.SHORT_SUFFIX}`,
-      VSCodeCommands.BUILD_BINARY,
-      VSCodeCommands.QUARKUS_WELCOME,
-      VSCodeCommands.DEPLOY_TO_OPENSHIFT,
-      `${VSCodeCommands.DEPLOY_TO_OPENSHIFT}${VSCodeCommands.SHORT_SUFFIX}`,
-      QuteClientCommandConstants.QUTE_VALIDATION_ENABLED_TOGGLE_OFF,
-      QuteClientCommandConstants.QUTE_VALIDATION_ENABLED_TOGGLE_ON,
-      QuteClientCommandConstants.REFACTOR_SURROUND_WITH_COMMENTS,
-      QuteClientCommandConstants.REFACTOR_SURROUND_WITH_CDATA,
-      QuteClientCommandConstants.REFACTOR_SURROUND_WITH_SECTION,
     ];
 
     expect(activationCommands).to.have.members(QUARKUS_ACTIVATION_COMMANDS);
